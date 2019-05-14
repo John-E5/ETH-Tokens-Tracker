8a484eaa7de7ee36a44b1dce42f31b706fad03c6
John Ennis <johnennis5@gmail.com>
2019-05-14 18:18:49 +0100

    Added Register Route to routes.py and added password hashing

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,10 @@ from flask_argon2 import Argon2
 from flask_login import LoginManager
 
 app = Flask(__name__, template_folder='templates')
-app.config['SECERT_KEY'] = '00ef221052a3fbe6fea58db709b5a93b'
+app.config['SECRET_KEY'] = 'f5e7522cec73e9e33b8c496a7e073f0d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+argon2 = Argon2(app)
 
 from app import routes
