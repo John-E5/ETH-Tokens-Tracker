86ba518bd5ea7f1ebe766d29826409d35d8b503d
John Ennis <johnennis5@gmail.com>
2019-05-14 17:05:35 +0100

    Added User class for registration to models.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,8 @@ from flask_argon2 import Argon2
 from flask_login import LoginManager
 
 app = Flask(__name__, template_folder='templates')
+app.config['SECERT_KEY'] = '00ef221052a3fbe6fea58db709b5a93b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 from app import routes
