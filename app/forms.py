ff345031551ccdaf3276ac62a2a309d22825a497
John Ennis <johnennis5@gmail.com>
2019-05-22 18:06:11 +0100

    Added profile page html template and removed submit form and replaced with submit button

--- app/forms.py
+++ app/forms.py
@@ -10,7 +10,6 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
@@ -26,13 +25,11 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
 
 
 class UpdateProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Update')
 
     def validate_username(self, username):
         if username.data != current_user.username:
