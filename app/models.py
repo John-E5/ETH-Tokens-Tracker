4d99241076421299474970f87c68c699c1a1ee57
John Ennis <johnennis5@gmail.com>
2019-05-23 11:43:57 +0100

    Added UserTokens db model to models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from app import db, login_manager
 from flask_login import UserMixin
 
@@ -12,6 +13,19 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    user_tokens = db.relationship('UsersTokens', backref='user', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
+
+
+class UsersTokens(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tokens = db.Column(db.String(100))
+    token_amount = db.Column(db.DECIMAL(8, 8))
+    token_price = db.Column(db.DECIMAL(8, 8))
+    buy_date = db.Column(db.Date, nullable=False, default=date)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"UsersTokens('{self.tokens}', '{self.token_amount}', '{self.token_price}', '{self.buy_date}')"
