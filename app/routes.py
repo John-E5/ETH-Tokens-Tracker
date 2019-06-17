3a9c12eea2516ca13fdce3e7badb50c543ad6837
John Ennis <johnennis5@gmail.com>
2019-06-17 18:48:09 +0100

    Added barchart and treemap to stats, updated and fixed styles

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ from app.models import User, UsersTokens
 
 # Pygal
 import pygal
-
+from pygal.style import DarkGreenBlueStyle
 
 # Index
 @app.route('/')
@@ -87,7 +87,7 @@ def profile_page():
 @login_required
 def dashboard():
     page = request.args.get('page', 1, type=int)
-    users_token = UsersTokens.query.order_by(UsersTokens.buy_date).paginate(page=page, per_page=6)
+    users_token = UsersTokens.query.filter_by(user=current_user).order_by(UsersTokens.buy_date).paginate(page=page, per_page=6)
 
     return render_template('dashboard.html', users_token=users_token)
 
@@ -158,13 +158,29 @@ def delete_token(id):
 
 
 @app.route('/portfolio_stats')
+@login_required
 def portfolio_stats():
-    # Piechart
+
+    # Load data
     user_token = dict(db.session.query(UsersTokens.tokens, UsersTokens.token_amount).filter_by(user=current_user).all())
-    pie_graph = pygal.Pie()
+
+    # Piechart
+    pie_graph = pygal.Pie(width=500, height=500, style=DarkGreenBlueStyle)
     for name, amount in user_token.items():
         pie_graph.add(name, amount)
     pie_graph = pie_graph.render_data_uri()
 
-    return render_template('portfolio_stats.html', pie_graph=pie_graph)
+    # Barchart
+    bar_graph = pygal.Bar(width=500, height=500, style=DarkGreenBlueStyle)
+    for name, amount in user_token.items():
+        bar_graph.add(name, amount)
+    bar_graph = bar_graph.render_data_uri()
+
+    # Treemap
+    Tree_map = pygal.Treemap(width=900, height=400, style=DarkGreenBlueStyle)
+    for name, amount in user_token.items():
+        Tree_map.add(name, amount)
+    Tree_map = Tree_map.render_data_uri()
+
+    return render_template('portfolio_stats.html', pie_graph=pie_graph, bar_graph=bar_graph, Tree_map=Tree_map)
 
