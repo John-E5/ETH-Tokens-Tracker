aea6bde3d8a857e20bc8c32af9d401dda62656fd
John <johnennis5@gmail.com>
2019-06-19 23:57:31 +0100

    Added postgres config for deployment

--- app/config.py
+++ app/config.py
@@ -8,3 +8,7 @@ class Config:
     load_dotenv(dotenv_path)
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASS = os.getenv('DB_PASS')
+    DB_HOST = os.getenv('DB_HOST')
