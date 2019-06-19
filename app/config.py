7260dc826831aa4ef1fdf0aa7d4e2b8013f67611
John <johnennis5@gmail.com>
2019-06-19 18:59:49 +0100

    Added dotenv to handle env vars

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
-    SECRET_KEY = 'f5e7522cec73e9e33b8c496a7e073f0d'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
