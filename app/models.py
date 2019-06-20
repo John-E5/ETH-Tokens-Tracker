4d9b4a3096fe9e1d2ccc1a5d85747bcdc06e88fc
John <johnennis5@gmail.com>
2019-06-20 10:33:27 +0100

    Updated db model to fix db error

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     user_tokens = db.relationship('UsersTokens', backref='user', lazy=True)
 
     def __repr__(self):
